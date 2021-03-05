52046307246e65fce28ace886ff1122f07fbc0dd
Luca Pasquali <luca@bucaniere.us>
2021-03-05 12:20:39 +0100

    deploy solr on private network, mount cifs share in place for data

--- azure/030_create_solr.sh
+++ azure/030_create_solr.sh
@@ -7,10 +7,13 @@ az container create \
     --image ${REGISTRY_NAME}.azurecr.io/${SOLR_IMAGE} \
     --registry-username ${REGISTRY_USERNAME} \
     --registry-password ${REGISTRY_PASSWORD} \
-    --dns-name-label $SOLR_HOST \
     --ports 8983 \
     --cpu 2 \
     --azure-file-volume-account-name $STORAGE_ACCOUNT_NAME \
     --azure-file-volume-account-key $STORAGE_KEY \
     --azure-file-volume-share-name $SOLR_SHARE_NAME \
-    --azure-file-volume-mount-path /opt/solr/server/solr/ckan/solr_data
+    --azure-file-volume-mount-path /opt/solr/server/solr/ckan/data \
+    --vnet privnet01 \
+    --vnet-address-prefix 10.0.0.0/16 \
+    --subnet default \
+    --subnet-address-prefix 10.0.0.0/24 
