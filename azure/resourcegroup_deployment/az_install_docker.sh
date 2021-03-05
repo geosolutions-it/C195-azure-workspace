da2bac774939ec42bbdb4a718144668db6576b9b
Luca Pasquali <luca@bucaniere.us>
2021-03-05 18:39:32 +0100

    added env variables to build ckan correctly

--- azure/resourcegroup_deployment/az_install_docker.sh
+++ azure/resourcegroup_deployment/az_install_docker.sh
@@ -31,7 +31,8 @@ smbPath=$(echo $httpEndpoint | cut -c7-$(expr length $httpEndpoint))$fileShareNa
 echo "$registryPassword" | sudo -u geosolutions docker login ${registryName}.azurecr.io --username $registryUsername --password-stdin 
 cd /home/geosolutions/C195-azure-workspace/ckan-docker
 sudo -u geosolutions docker-compose --env-file ../azure/resourcegroup_deployment/ckan-compose/.env.sample build --no-cache ckan ckan_solr && docker-compose --env-file ../azure/resourcegroup_deployment/ckan-compose/.env.sample push ckan ckan_solr
-
+cd /home/geosolutions/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose
+sudo -u geosolutions docker-compose --env-file .env.sample up -d ckan
 # mount ckan share
 
 sudo mkdir -p $mntPath
