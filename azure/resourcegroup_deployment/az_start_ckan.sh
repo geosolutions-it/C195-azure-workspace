ea28f74101d34ded870f06a55ccdf60ce451cf3a
Luca Pasquali <luca@bucaniere.us>
2021-03-10 18:05:38 +0100

    script fixes

--- azure/resourcegroup_deployment/az_start_ckan.sh
+++ azure/resourcegroup_deployment/az_start_ckan.sh
@@ -8,8 +8,8 @@ registryPassword="$arg6"
 httpEndpoint="$arg7"
 
 cd /home/geosolutions/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose
-if [ ! -f .env ]; then
-    cp .env-sample .env
+if [ ! -f /home/geosolutions/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose/.env ]; then
+    cp /home/geosolutions/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose/.env-sample /home/geosolutions/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose/.env
 fi    
 sudo -u geosolutions docker-compose --env-file .env down
 sudo -u geosolutions docker-compose --env-file .env up -d ckan
