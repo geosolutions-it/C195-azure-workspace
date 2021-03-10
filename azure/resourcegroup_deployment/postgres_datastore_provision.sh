92f69ba34320bf8d6cefae0105fd18391a79fcb8
Luca Pasquali <luca@bucaniere.us>
2021-03-10 17:54:34 +0100

    script fixes

--- azure/resourcegroup_deployment/postgres_datastore_provision.sh
+++ azure/resourcegroup_deployment/postgres_datastore_provision.sh
@@ -3,5 +3,6 @@
 
 export PGPASSWORD=$arg2
 psql -U $arg1@$arg3 -h $arg4 postgres -c 'create database ckan with owner ckan;'
+psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD ${arg5};"
 psql -U $arg1@$arg3 -h $arg4 ckan -f /home/geosolutions/C195-azure-workspace/azure/azure_pg_test/00_create_datastore.sql
 psql -U $arg1@$arg3 -h $arg4 datastore -f /home/geosolutions/C195-azure-workspace/azure/azure_pg_test/20_postgis_permissions.sql
