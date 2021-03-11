5dd02dac5fd5ef67b6d8c7ec997c7e01965d1f21
Luca Pasquali <luca@bucaniere.us>
2021-03-11 10:16:25 +0100

    pg_backup script

--- azure/resourcegroup_deployment/az_scripts/az_postgres_backup.sh
+++ azure/resourcegroup_deployment/az_scripts/az_postgres_backup.sh
@@ -5,6 +5,8 @@ mkdir -p ${CKAN_SHARE_MOUNT}/database_backup
 DUMP_DATE=$(date +%F_%R)
 CKAN_DUMP_FILE=ckan_${DUMP_DATE}.sql
 DATASTORE_DUMP_FILE=datastore_${DUMP_DATE}.sql
+POSTRGRES_DUMP_FILE=postgres_${DUMP_DATE}.sql
 
 pg_dump -U $arg1@$arg3 -h $arg4 ckan -f ${CKAN_SHARE_MOUNT}/database_backup/${CKAN_DUMP_FILE}
 pg_dump -U $arg1@$arg3 -h $arg4 datastore -f ${CKAN_SHARE_MOUNT}/database_backup/${DATASTORE_DUMP_FILE}
+pg_dump -U $arg1@$arg3 -h $arg4 postgres -f  ${CKAN_SHARE_MOUNT}/database_backup/${POSTRGRES_DUMP_FILE}
