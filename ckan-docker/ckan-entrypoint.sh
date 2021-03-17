b0dfbda6c6fbee9fb8fd5c9db7b335a673b383bb
Luca Pasquali <luca@bucaniere.us>
2021-03-17 16:42:22 +0100

    correction top sqlalchemy values in crudini commands

--- ckan-docker/ckan-entrypoint.sh
+++ ckan-docker/ckan-entrypoint.sh
@@ -60,11 +60,11 @@ fi
 
 # changes to the ini file -- SHOULD BE IDEMPOTENT
 
-crudini --set --verbose ${CONFIG_INI} app:main pool_size 10
-crudini --set --verbose ${CONFIG_INI} app:main echo_pool True
-crudini --set --verbose ${CONFIG_INI} app:main pool_pre_ping True
-crudini --set --verbose ${CONFIG_INI} app:main pool_reset_on_return rollback
-crudini --set --verbose ${CONFIG_INI} app:main pool_timeout 30
+crudini --set --verbose ${CONFIG_INI} app:main sqlalchemy.pool_size 10
+crudini --set --verbose ${CONFIG_INI} app:main sqlalchemy.echo_pool True
+crudini --set --verbose ${CONFIG_INI} app:main sqlalchemy.pool_pre_ping True
+crudini --set --verbose ${CONFIG_INI} app:main sqlalchemy.pool_reset_on_return rollback
+crudini --set --verbose ${CONFIG_INI} app:main sqlalchemy.pool_timeout 30
 crudini --set --verbose ${CONFIG_INI} DEFAULT debug True
 crudini --set --verbose ${CONFIG_INI} logger_root level DEBUG
 crudini --set --verbose ${CONFIG_INI} logger_werkzeug level DEBUG
