83e99e7d1033053b355b92fccef54ba245bbf3a5
Luca Pasquali <luca@bucaniere.us>
2021-03-30 17:08:31 +0200

    environment fixes

--- azure/resourcegroup_deployment/azure_solr_config.sh
+++ azure/resourcegroup_deployment/azure_solr_config.sh
@@ -22,6 +22,6 @@ az container create \
 
 # Start ckan composition
 
-# az vm run-command invoke -g ${RESOURCE_GROUP} -n ${CKAN_VM_NAME} --command-id RunShellScript --scripts @./az_scripts/az_postgres_datastore_provision.sh --parameters arg1="$CKAN_PG_USER_PARTIAL" arg2="$POSTGRES_PASSWORD" arg3="$PG_HOST" arg4="$PG_HOST_FULL" arg5="${DATASTORE_READONLY_PASSWORD}"
+az vm run-command invoke -g ${RESOURCE_GROUP} -n ${CKAN_VM_NAME} --command-id RunShellScript --scripts @./az_scripts/az_postgres_datastore_provision.sh --parameters arg1="$CKAN_PG_USER_PARTIAL" arg2="$POSTGRES_PASSWORD" arg3="$PG_HOST" arg4="$PG_HOST_FULL" arg5="${DATASTORE_READONLY_PASSWORD}"
 
-# az vm run-command invoke -g ${RESOURCE_GROUP} -n ${CKAN_VM_NAME} --command-id RunShellScript --scripts @./az_scripts/az_start_ckan.sh --parameters arg1="$RESOURCE_GROUP" arg2="$STORAGE_ACCOUNT_NAME" arg3="$CKAN_SHARE_NAME" arg4="$REGISTRY_NAME" arg5="$REGISTRY_USERNAME" arg6="$REGISTRY_PASSWORD" arg7="$httpEndpoint" arg8="$STORAGE_KEY"
+az vm run-command invoke -g ${RESOURCE_GROUP} -n ${CKAN_VM_NAME} --command-id RunShellScript --scripts @./az_scripts/az_start_ckan.sh --parameters arg1="$RESOURCE_GROUP" arg2="$STORAGE_ACCOUNT_NAME" arg3="$CKAN_SHARE_NAME" arg4="$REGISTRY_NAME" arg5="$REGISTRY_USERNAME" arg6="$REGISTRY_PASSWORD" arg7="$httpEndpoint" arg8="$STORAGE_KEY"
