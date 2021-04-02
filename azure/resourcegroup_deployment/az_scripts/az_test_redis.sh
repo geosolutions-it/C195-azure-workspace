5ad3250b641c52f05ad43bd94ea09cdbe2bbaa02
Luca Pasquali <luca@bucaniere.us>
2021-04-02 15:13:00 +0200

    SMOKE tests

--- azure/resourcegroup_deployment/az_scripts/az_test_redis.sh
+++ azure/resourcegroup_deployment/az_scripts/az_test_redis.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
-set -x
+
 REDIS_AUTHKEY=$arg1
 REDIS_HOST_FULL=$arg2
 RESULT=`(printf "AUTH $REDIS_AUTHKEY\r\nPING\r\nQUIT\r\n") | nc $REDIS_HOST_FULL 6379 | grep PONG`
-[[ "$RESULT" != "" ]] && ( echo "Redis instance at $REDIS_HOST_FULL is working!" ) || ( echo "Redis istance at $REDIS_HOST_FULL has problems!" )
+[ "$RESULT" != "" ] && ( echo "Redis instance at $REDIS_HOST_FULL is working!" ) || ( echo "Redis istance at $REDIS_HOST_FULL has problems!" )
