400e4783c2ac2e0db2d68429c4e8b43424c49f36
Luca Pasquali <luca@bucaniere.us>
2021-04-06 14:53:54 +0200

    fixed gdb addition to docker restart #16

--- ckan-docker/capture_gdb.sh
+++ ckan-docker/capture_gdb.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DATE=$(date '+%Y-%m-%d')
-PP=$(pgrep -ofa host)
-gdb -x gdb.commands /usr/lib/ckan/venv/bin/python3 $PP  > /var/lib/ckan/$DATE_gdb_ckan.txt
+DATE=$(date '+%Y-%m-%d-%H%M')
+PP=$(pgrep -ofa host | awk '{print $1}')
+gdb -x gdb.commands /usr/lib/ckan/venv/bin/python3 $PP  > /var/lib/ckan/${DATE}_gdb_ckan.txt
