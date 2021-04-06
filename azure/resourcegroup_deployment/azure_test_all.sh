f6ae33f4bd166f6e2fa1ace2b2e364978842491b
Luca Pasquali <luca@bucaniere.us>
2021-04-06 16:11:32 +0200

    master deployment test - fixes

--- azure/resourcegroup_deployment/azure_test_all.sh
+++ azure/resourcegroup_deployment/azure_test_all.sh
@@ -4,7 +4,7 @@ source ./setenv.sh
 
 ## Test ckan up using testing primer ()
 ckan_up() {
-    TEST_URL="https://ckan-vmtest3.westeurope.cloudapp.azure.com/user/default"
+    TEST_URL="${CKAN_SITE_URL}/user/default"
     RESULT=$(curl -I -s $TEST_URL | head -1)
     [[ "$RESULT" == *"200"* ]] && echo "CKAN is up and running" || echo "CKAN has problems"
 }
@@ -22,18 +22,18 @@ redis_up() {
 }
 
 ckan_works() {
-    TEST_URL="https://ckan-vmtest3.westeurope.cloudapp.azure.com/testing/primer"
+    TEST_URL="${CKAN_SITE_URL}/testing/primer"
     RESULT=$(curl -I -s $TEST_URL | head -1)
     [[ "$RESULT" == *"200"* ]] && echo "CKAN front-end services are working!" || echo "CKAN front-end services have problems"
 }
 
 ckan_admin() {
-    TEST_URL="https://ckan-vmtest3.westeurope.cloudapp.azure.com/user/admin"
+    TEST_URL="${CKAN_SITE_URL}/user/admin"
     RESULT=$(curl -I -s $TEST_URL | head -1)
     [[ "$RESULT" == *"200"* ]] && echo "CKAN admin was correctly created" || echo "CKAN admin was not created"
 }
 ckan_null_user() {
-    TEST_URL="https://ckan-vmtest3.westeurope.cloudapp.azure.com/euser/null"
+    TEST_URL="${CKAN_SITE_URL}/user/null"
     RESULT=$(curl -I -s $TEST_URL | head -1)
     [[ "$RESULT" == *"404"* ]] && echo "CKAN 404 test working!" || echo "CKAN has problems - user should not exist at install time"
 }
