00b177eadcf6c0e4e16a078d7d70042872b3ccf5
Luca Pasquali <luca@bucaniere.us>
2021-05-11 16:41:36 +0200

    changed psql command

--- ckan-docker/ckan-entrypoint.sh
+++ ckan-docker/ckan-entrypoint.sh
@@ -97,7 +97,7 @@ crudini --set --verbose ${CONFIG_INI} app:main ckan.datapusher.assume_task_stale
 
 #Configure datastore SQL functions
 
-$CKAN_VENV/bin/ckan -c ${CONFIG_INI} datastore set-permissions | grep -v ^"$(date +%Y-%m-%d)" | psql --set ON_ERROR_STOP=1 -U datastore_ro%40${PG_HOST} -h ${PG_HOST_FULL}
+$CKAN_VENV/bin/ckan -c ${CONFIG_INI} datastore set-permissions | grep -v ^"$(date +%Y-%m-%d)" | psql --set ON_ERROR_STOP=1 -U datastore_ro@${PG_HOST} -h ${PG_HOST_FULL}
 
 # END changes to the ini file 
 
