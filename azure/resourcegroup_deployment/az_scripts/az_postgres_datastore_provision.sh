cf2dd812caae515c6b92dbd4fde44c3078cb1445
Luca Pasquali <luca@bucaniere.us>
2021-05-11 15:52:07 +0200

    datastore db provision

--- azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
+++ azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
@@ -6,5 +6,7 @@ VMUSER=$arg6
 
 psql -U $arg1@$arg3 -h $arg4 postgres -c 'create database ckan with owner ckan;'
 psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '${arg5}';"
-psql -U $arg1@$arg3 -h $arg4 ckan -f /home/$VMUSER/C195-azure-workspace/azure/azure_pg_test/00_create_datastore.sql
-psql -U $arg1@$arg3 -h $arg4 datastore -f /home/$VMUSER/C195-azure-workspace/azure/azure_pg_test/20_postgis_permissions.sql
+psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';"
+psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;"
+psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT SELECT ON ALL TABLES IN DATABASE datastore TO datastore_ro;"
+psql -U $arg1@$arg3 -h $arg4 datastore -c "CREATE EXTENSION IF NOT EXISTS postgis; ALTER VIEW geometry_columns OWNER TO ckan; ALTER TABLE spatial_ref_sys OWNER TO ckan;"
