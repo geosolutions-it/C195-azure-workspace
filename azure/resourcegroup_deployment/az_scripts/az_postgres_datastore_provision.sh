9214235897423fa38949392756bd3224ae6027aa
Luca Pasquali <luca@bucaniere.us>
2021-05-11 17:36:32 +0200

    changed psql command

--- azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
+++ azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
@@ -5,7 +5,7 @@ export PGPASSWORD=$arg2
 VMUSER=$arg6
 
 psql -U $arg1@$arg3 -h $arg4 postgres -c 'create database ckan with owner ckan;'
-psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro@${arg3} NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '${arg5}';"
+psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN PASSWORD '${arg5}';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE DATABASE datastore OWNER ckan ENCODING 'utf-8';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT SELECT ON ALL TABLES IN DATABASE datastore TO datastore_ro;"
