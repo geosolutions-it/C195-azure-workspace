a7e9b33ffce8518aa4d0ef36e79ff01e9eb628e0
Luca Pasquali <luca@bucaniere.us>
2021-05-28 12:30:39 +0200

    fixing datastore_ro privileges

--- azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
+++ azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
@@ -5,8 +5,8 @@ export PGPASSWORD=$arg2
 VMUSER=$arg6
 
 psql -U $arg1@$arg3 -h $arg4 postgres -c 'create database ckan with owner ckan;'
-psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE IF NOT EXISTS \"datastore_ro@$arg3\" LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION PASSWORD \'${arg5}\';"
-psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE IF NOT EXISTS datastore_ro LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION PASSWORD '${arg5}';"
+psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE \"datastore_ro@$arg3\" LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION PASSWORD \'${arg5}\';"
+psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION PASSWORD '${arg5}';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE DATABASE datastore with OWNER ckan ENCODING 'utf-8';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;"
 psql -U $arg1@$arg3 -h $arg4 datastore -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO datastore_ro"
