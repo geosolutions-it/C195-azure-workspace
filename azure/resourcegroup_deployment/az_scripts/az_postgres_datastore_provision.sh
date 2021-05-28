b63f6ebd7811f499673a28c27ec186b3cf3922af
Luca Pasquali <luca@bucaniere.us>
2021-05-28 10:15:23 +0200

    datastore setup

--- azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
+++ azure/resourcegroup_deployment/az_scripts/az_postgres_datastore_provision.sh
@@ -6,7 +6,10 @@ VMUSER=$arg6
 
 psql -U $arg1@$arg3 -h $arg4 postgres -c 'create database ckan with owner ckan;'
 psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE \"datastore_ro@$arg3\" NOCREATEDB NOCREATEROLE LOGIN PASSWORD '${arg5}';"
+#psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE ROLE datastore_ro NOCREATEDB NOCREATEROLE LOGIN PASSWORD '${arg5}';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "CREATE DATABASE datastore with OWNER ckan ENCODING 'utf-8';"
 psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT ALL PRIVILEGES ON DATABASE datastore TO ckan;"
-psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT SELECT ON ALL TABLES IN DATABASE datastore TO \"datastore_ro@$arg3\";"
+#psql -U $arg1@$arg3 -h $arg4 postgres -c "GRANT SELECT ON ALL TABLES IN DATABASE datastore TO \"datastore_ro@$arg3\";"
+psql -U $arg1@$arg3 -h $arg4 datastore -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"datastore_ro@$arg3\";"
+psql -U $arg1@$arg3 -h $arg4 datastore -c "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO \"datastore_ro@$arg3\";"
 psql -U $arg1@$arg3 -h $arg4 datastore -c "CREATE EXTENSION IF NOT EXISTS postgis; ALTER VIEW geometry_columns OWNER TO ckan; ALTER TABLE spatial_ref_sys OWNER TO ckan;"
