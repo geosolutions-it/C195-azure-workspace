185be74453e6550796fd45561cff35db3eb89300
Luca Pasquali <luca@bucaniere.us>
2021-12-01 16:13:31 +0100

    test ssl certificates automation - reset parameters

--- azure/resourcegroup_deployment/az_scripts/az_restart_services.sh
+++ azure/resourcegroup_deployment/az_scripts/az_restart_services.sh
@@ -18,7 +18,6 @@ sudo -u ${VMUSER} docker-compose --env-file .env down
 sudo -u ${VMUSER} docker-compose --env-file .env up -d
 
 if [ -f /home/${VMUSER}/C195-azure-workspace/azure/resourcegroup_deployment/custom-ssl/privkey.pem ]; then
-
     sudo cp /home/${VMUSER}/C195-azure-workspace/azure/resourcegroup_deployment/custom-ssl/nginx-default /home/${VMUSER}/C195-azure-workspace/azure/resourcegroup_deployment/ckan-compose/site-confs/default
     sudo -u ${VMUSER} docker-compose --env-file .env down
     sudo -u ${VMUSER} docker-compose --env-file .env up -d
